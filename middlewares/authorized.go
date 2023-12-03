14cdccd622f66087528605fe252be490efe8d3b6
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-03 21:04:32 +0600

    auth middleware
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- middlewares/authorized.go
+++ middlewares/authorized.go
@@ -3,15 +3,24 @@ package middlewares
 import (
 	"strings"
 
-	"github.com/faysalahmed-dev/wherehouse-order-picklist/db"
-	"github.com/faysalahmed-dev/wherehouse-order-picklist/ent"
-	"github.com/faysalahmed-dev/wherehouse-order-picklist/ent/user"
+	"github.com/faysalahmed-dev/wherehouse-order-picklist/db/schema"
+	"github.com/faysalahmed-dev/wherehouse-order-picklist/db/store"
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/helpers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
 
-func Authorized(c *fiber.Ctx) error {
+type AuthMiddleware struct {
+	userStore store.UserStore
+}
+
+func NewAuthHandler(userStore store.UserStore) *AuthMiddleware {
+	return &AuthMiddleware{
+		userStore: userStore,
+	}
+}
+
+func (h *AuthMiddleware) Authorized(c *fiber.Ctx) error {
 	token := c.Get("authorization", "")
 	if len(token) > 0 && strings.Contains(token, "Bearer ") {
 		t := strings.Split(token, " ")
@@ -33,20 +42,20 @@ func Authorized(c *fiber.Ctx) error {
 	if err != nil {
 		return fiber.NewError(fiber.StatusUnauthorized, "user not found")
 	}
-	user, err := db.DBClient.User.Query().Where(user.ID(user_id)).First(c.Context())
+	user, err := h.userStore.GetUserById(user_id.String())
 	if err != nil {
 		return fiber.NewError(fiber.StatusUnauthorized, "user not found")
 	}
-	if user.Blocked {
-		return fiber.NewError(fiber.StatusForbidden, "account blocked")
-	}
+	// if user.Blocked {
+	// 	return fiber.NewError(fiber.StatusForbidden, "account blocked")
+	// }
 	c.Locals("user", user)
 
 	return c.Next()
 }
 
-func AdminOnly(c *fiber.Ctx) error {
-	u, _ := c.Locals("user").(*ent.User)
+func (h *AuthMiddleware) AdminOnly(c *fiber.Ctx) error {
+	u, _ := c.Locals("user").(*schema.User)
 
 	if u.Type != "ADMIN" {
 		return fiber.NewError(fiber.StatusForbidden, "permission denied")
