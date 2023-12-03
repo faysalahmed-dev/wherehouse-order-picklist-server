2bac1a77ac5454bf4f0bfa80d0fff665d5351709
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-04 03:43:00 +0600

    refactor: category schema and user relationship
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- db/schema/category.go
+++ db/schema/category.go
@@ -5,24 +5,42 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Category struct {
-	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
-	Name      string    `json:"name"`
-	Value     string    `gorm:"unique" json:"value"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string     `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
+	Name      string     `json:"name"`
+	Value     string     `gorm:"unique" json:"value"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+
+	UserId string `gorm:"type:uuid;" json:"userId,omitempty"`
+	User   *User  `json:"user,omitempty"`
+}
+
+func (c *Category) BeforeCreate(tx *gorm.DB) error {
+	c.ID = uuid.New().String()
+	return nil
 }
 
 type CreateCategoryPayload struct {
 	Name string `json:"name" validate:"required,min=1,max=15"`
 }
 
-func CreateCategoryParams(c CreateCategoryPayload) (*Category, error) {
+func CreateCategoryParams(c CreateCategoryPayload, userId string) *Category {
+	val := strings.ToLower(strings.Join(strings.Split(c.Name, " "), "-"))
+	return &Category{
+		Name:   c.Name,
+		Value:  val,
+		UserId: userId,
+	}
+}
+
+func UpdateCategoryParams(c CreateCategoryPayload) *Category {
 	val := strings.ToLower(strings.Join(strings.Split(c.Name, " "), "-"))
 	return &Category{
 		Name:  c.Name,
 		Value: val,
-	}, nil
+	}
 }
