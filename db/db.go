7d777dd3f44f0e30364c7de56dcadc98bd561b6d
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-03 19:03:25 +0600

    migrate to gorm
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- db/db.go
+++ db/db.go
@@ -1,34 +1,32 @@
 package db
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/faysalahmed-dev/wherehouse-order-picklist/ent"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var DBClient *ent.Client
+var DBClient *gorm.DB
 
-func ConnectToDB() *ent.Client {
+func ConnectToDB() *gorm.DB {
 	HOST := os.Getenv("DB_HOST")
 	PORT := os.Getenv("DB_PORT")
 	USER := os.Getenv("DB_USER")
 	DB_NAME := os.Getenv("DB_NAME")
 	PASSWORD := os.Getenv("DB_PASSWORD")
 
-	dbURL := fmt.Sprintf("host=%v port=%v user=%v dbname=%v password=%v sslmode=disable", HOST, PORT, USER, DB_NAME, PASSWORD)
+	dbURI := fmt.Sprintf("host=%v port=%v user=%v dbname=%v password=%v sslmode=disable", HOST, PORT, USER, DB_NAME, PASSWORD)
 
-	client, err := ent.Open("postgres", dbURL)
+	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed opening connection to postgres: %v", err)
 	}
-	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
-	}
-	DBClient = client
+	// db.AutoMigrate(&Product{})
+	DBClient = db
 	fmt.Println("database connected...")
-	return client
+	return db
 }
