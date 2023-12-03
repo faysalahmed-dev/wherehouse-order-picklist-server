65669fe63442b556e7976d88676df215d987dd87
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-04 03:39:46 +0600

    refactor: uuid and hash password
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- db/schema/user.go
+++ db/schema/user.go
@@ -1,20 +1,34 @@
 package schema
 
 import (
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `gorm:"unique" json:"email"`
-	Password  string    `json:"-"`
-	Type      string    `json:"type"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string     `gorm:"primary_key;column:id;" json:"id"`
+	Name      string     `json:"name"`
+	Email     string     `gorm:"unique" json:"email,omitempty"`
+	Password  string     `json:"-"`
+	Type      string     `json:"type,omitempty"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+
+	Categories []Category `gorm:"foreignKey:UserId;constraint:OnDelete:CASCADE" json:"categories,omitempty"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) error {
+	user.ID = uuid.New().String()
+	bytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
 }
 
 type RegisterUserPayload struct {
@@ -28,20 +42,11 @@ type LoginUserPayload struct {
 	Password string `json:"password" validate:"required,min=6,max=255"`
 }
 
-func CreateNewUserParams(p RegisterUserPayload) (*User, error) {
-	// err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	bytes, err := bcrypt.GenerateFromPassword([]byte(p.Password), 14)
-	if err != nil {
-		return nil, err
-	}
+func CreateNewUserParams(p RegisterUserPayload) *User {
 	return &User{
 		Name:     p.Name,
-		Email:    p.Email,
-		Password: string(bytes),
+		Email:    strings.ToLower(p.Email),
+		Password: p.Password,
 		Type:     "USER",
-	}, nil
-}
-
-func ComparePassword(userPassword string, savedPassword string) error {
-	return bcrypt.CompareHashAndPassword([]byte(savedPassword), []byte(userPassword))
+	}
 }
