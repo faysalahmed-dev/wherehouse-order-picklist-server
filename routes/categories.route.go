db8bff369724e5eb24d8610f90c3a3cbe5ba3593
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-04 19:55:59 +0600

    refactor: take db store
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- routes/categories.route.go
+++ routes/categories.route.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-	"github.com/faysalahmed-dev/wherehouse-order-picklist/db/store"
+	"github.com/faysalahmed-dev/wherehouse-order-picklist/db"
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/handlers"
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
-func RegisterCategoriesRoutes(r fiber.Router, s *store.CategoryStore, u *store.UserStore) {
-	h := handlers.NewCategoryHandler(*s)
-	authM := middlewares.NewAuthHandler(*u)
+func RegisterCategoriesRoutes(r fiber.Router, s *db.Store) {
+	h := handlers.NewCategoryHandler(s.Category)
+	authM := middlewares.NewAuthHandler(s.User)
 	router := r.Group("/categories")
 	router.Use(authM.Authorized)
 	router.Get("", h.GetCategories)
--- routes/users.routes.go
+++ routes/users.routes.go
@@ -1,19 +1,19 @@
 package routes
 
 import (
-	"github.com/faysalahmed-dev/wherehouse-order-picklist/db/store"
+	"github.com/faysalahmed-dev/wherehouse-order-picklist/db"
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/handlers"
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
-func RegisterUserRoutes(r fiber.Router, s *store.UserStore) {
-	h := handlers.NewUserHandler(*s)
+func RegisterUserRoutes(r fiber.Router, s *db.Store) {
+	h := handlers.NewUserHandler(s.User)
 	userRoute := r.Group("/user")
 	userRoute.Post("/login", h.LoginUser)
 	userRoute.Post("/register", h.RegisterUser)
 
-	authM := middlewares.NewAuthHandler(*s)
+	authM := middlewares.NewAuthHandler(s.User)
 	userRoute.Use(authM.Authorized)
 	userRoute.Get("/profile", h.Profile)
 	userRoute.Use(authM.AdminOnly)
