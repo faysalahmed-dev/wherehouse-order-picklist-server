4514ac0c554e9228cd1ef62c4e71d2cdfdcdb37b
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-04 14:17:16 +0600

    update category routes
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- routes/categories.route.go
+++ routes/categories.route.go
@@ -1,17 +1,20 @@
 package routes
 
 import (
+	"github.com/faysalahmed-dev/wherehouse-order-picklist/db/store"
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/handlers"
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
-func RegisterCategoriesRoutes(r fiber.Router) {
+func RegisterCategoriesRoutes(r fiber.Router, s *store.CategoryStore, u *store.UserStore) {
+	h := handlers.NewCategoryHandler(*s)
+	authM := middlewares.NewAuthHandler(*u)
 	router := r.Group("/categories")
-	router.Use(middlewares.Authorized)
-	router.Get("", handlers.GetCategories)
-	router.Get("/options", handlers.GetCategoriesOptions)
-	router.Post("/add-category", handlers.CreateCategory)
-	router.Patch("/edit/:id", handlers.UpdateCategory)
-	router.Delete("/:id", handlers.DeleteCategory)
+	router.Use(authM.Authorized)
+	router.Get("", h.GetCategories)
+	router.Post("/add-category", h.CreateCategory)
+	router.Get("/options", h.GetCategoriesOptions)
+	router.Patch("/edit/:id", h.UpdateCategory)
+	router.Delete("/:id", h.DeleteCategory)
 }
