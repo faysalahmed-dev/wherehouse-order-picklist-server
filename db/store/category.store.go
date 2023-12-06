38a862aa1c42cc7a42c46b2a5344d171cdeca3ac
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-06 18:15:45 +0600

    fix: check item exists
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- db/store/category.store.go
+++ db/store/category.store.go
@@ -2,7 +2,6 @@ package store
 
 import (
 	"errors"
-	"fmt"
 	"math"
 
 	"github.com/faysalahmed-dev/wherehouse-order-picklist/db/schema"
@@ -33,7 +32,6 @@ func NewCategoryStore(client *gorm.DB) *DBCategoryStore {
 func (s *DBCategoryStore) Pagination(limit int) (int, error) {
 	var count int64
 	err := s.client.Model(&schema.Category{}).Count(&count).Error
-	fmt.Println(err)
 	if err != nil {
 		return 0, errors.New("unable to count record")
 	}
@@ -43,12 +41,12 @@ func (s *DBCategoryStore) GetCategories(page int, limit int) (*[]schema.Category
 	var c []schema.Category
 	o := (page - 1) * limit
 	err := s.client.Model(&schema.Category{}).
-		Select("id", "name", "value", "user", "user_id").
+		Select("id", "name", "value", "user_id").
 		Limit(limit).
 		Offset(o).
 		Order("created_at desc").
 		Preload("User", func(db *gorm.DB) *gorm.DB {
-			return db.Select("id", "name")
+			return db.Select("id", "name", "type")
 		}).
 		Find(&c).Error
 	if err != nil {
@@ -66,7 +64,6 @@ func (s *DBCategoryStore) GetCategoryOptions(page int, limit int) (*[]schema.Cat
 		Offset(o).
 		Order("created_at desc").
 		Find(&c).Error
-	fmt.Println(err)
 	if err != nil {
 		return nil, errors.New(err.Error())
 	}
@@ -82,11 +79,19 @@ func (s *DBCategoryStore) InsertCategory(c *schema.Category) (*schema.Category, 
 }
 
 func (s *DBCategoryStore) DeleteById(id string) error {
-	return s.client.Unscoped().Delete(&schema.Category{}, id).Error
+	count := s.client.Where("id = ?", id).Unscoped().Delete(&schema.Category{}).RowsAffected
+	if count == 0 {
+		return errors.New("item not found")
+	}
+	return nil
 }
 
 func (s *DBCategoryStore) DeleteByUserAndId(userId string, id string) error {
-	return s.client.Where("user_id = ? AND id = ?", userId, id).Unscoped().Delete(&schema.Category{}).Error
+	count := s.client.Where("user_id = ? AND id = ?", userId, id).Unscoped().Delete(&schema.Category{}).RowsAffected
+	if count == 0 {
+		return errors.New("item not found")
+	}
+	return nil
 }
 
 func (s *DBCategoryStore) GetByFields(c *schema.Category) (*schema.Category, error) {
@@ -97,7 +102,9 @@ func (s *DBCategoryStore) GetByFields(c *schema.Category) (*schema.Category, err
 
 func (s *DBCategoryStore) UpdateById(id string, c *schema.Category) (*schema.Category, error) {
 	var result *schema.Category
-	r := s.client.Model(&schema.Category{}).Where("id = ?", id).Updates(c)
-
-	return result, r.Error
+	r := s.client.Model(&schema.Category{}).Where("id = ?", id).Updates(c).Scan(&result)
+	if r.RowsAffected == 0 {
+		return nil, errors.New("record not found")
+	}
+	return result, nil
 }
